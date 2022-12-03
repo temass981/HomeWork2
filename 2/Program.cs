8ab30d3758f028f6c74afd2869d6b70e1b5803fb
Artyom <simstemasss@mail.ru>
2022-12-03 21:49:14 +0300

    Second task

---
+++ 2/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Введите любое число");
+int a = int.Parse(Console.ReadLine());
+int b = 0;
+if (a>1000){
+    while(a>1000){
+        a=a/10;
+        b=a%10;
+       
+    }
+     System.Console.WriteLine(b);
+    
+}else if (a>100 & a<1000){
+    b=a%10;
+    System.Console.WriteLine(b);
+}else if (a<100){
+    System.Console.WriteLine("Третьей цифры нет");
+}
