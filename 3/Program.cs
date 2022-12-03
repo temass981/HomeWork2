1f982e9e5b8aaf768faf853db65df17944f6fa7d
Artyom <simstemasss@mail.ru>
2022-12-03 21:56:36 +0300

    three task

---
+++ 3/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите день недели цифрой от 1 до 7");
+int s = int.Parse(Console.ReadLine());
+switch (s){
+    case 1:
+    Console.WriteLine("Понедельник");
+    break;
+     case 2:
+    Console.WriteLine("Вторник");
+    break;
+     case 3:
+    Console.WriteLine("Среда");
+    break;
+     case 4:
+    Console.WriteLine("Четверг");
+    break;
+     case 5:
+    Console.WriteLine("Пятница");
+    break;
+     case 6:
+    Console.WriteLine("Суббота");
+    break;
+     case 7:
+    Console.WriteLine("Воскресенье");
+    break;}
+if (s==6 || s==7)
+    System.Console.WriteLine("Это выходной");
+else System.Console.WriteLine("Это не выходной");
